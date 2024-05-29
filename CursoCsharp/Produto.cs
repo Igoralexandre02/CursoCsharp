9fb92985f8ea023d2c5458a81ee82f5f96e068cf
Igor Vieira <igoralexandrevieira652@gmail.com>
2024-05-29 19:13:14 -0400

    Primeiro commit neste projeto.

---
+++ CursoCsharp/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace CursoCsharp
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+        public void AddProduto(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotal().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
