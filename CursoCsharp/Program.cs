98442e228f6bcd5a8159014a6d220499ca675c56
Igor Vieira <igoralexandrevieira652@gmail.com>
2024-06-03 19:03:21 -0400

    [Csharp][Secao-4] Exercicio proposto 4(menbros estaticos)

--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -7,25 +7,13 @@ namespace CursoCsharp
     {
         static void Main(string[] args)
         {
-            var A1 = new Aluno();
-            Console.Write("Nome do aluno: ");
-            A1.Nome = Console.ReadLine();
-            Console.WriteLine("Digite as tres notas do aluno: ");
-            A1.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            A1.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            A1.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            if (A1.NotaTotal() >= 60.00)
-            {
-                Console.WriteLine("APROVADO!");
-                Console.WriteLine("Nota final: " + A1.NotaTotal().ToString("F2", CultureInfo.InvariantCulture));
-            }
-            else
-            {
-                double pendencia = 60.00 - A1.NotaTotal();
-                Console.WriteLine("REPROVADO!");
-                Console.WriteLine("Faltaram: " + pendencia.ToString("F2", CultureInfo.InvariantCulture));
-            }
+            Console.Write("Informe a cotação do dólar: ");
+            var cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Informe o valor que irá comprar: ");
+            var dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            var valorTotal = ConversorDeMoedas.Conversor(cotacao, dolar);
+            Console.WriteLine("Valor a ser pago em reais: "+ valorTotal.ToString("F2", CultureInfo.InvariantCulture)); 
         }
     }
 }
