248364256e065635c330c6ac9754abbfc9b167ff
Igor Vieira <igoralexandrevieira652@gmail.com>
2024-06-04 21:54:52 -0400

    [Csharp][Secao-5] Exercicio 1

---
+++ CursoCsharp/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+namespace CursoCsharp
+{
+    internal class ContaBancaria
+    {
+        public string Nome { get; private set; }
+        public int Nconta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int nconta, string nome)
+        {
+            Nconta = nconta;
+            Nome = nome;
+        }
+
+        public double Deposito(double V)
+        {
+            return Saldo += V;
+        }
+        
+        public double Saque(double V)
+        {
+            return Saldo -= V + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " 
+                + Nconta 
+                + ", Titular: " 
+                + Nome 
+                + ", Saldo: $" 
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
